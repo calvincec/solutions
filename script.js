04a23343203c729e5cd2612e59560cb4b2446837
Calvince Ndemo <calyndemo16@gmail.com>
2024-12-08 18:19:56 +0300

    Initial work

---
+++ script.js
@@ -0,0 +1,14 @@
+
+
+const button = document.getElementById('colorButton');
+
+
+const colors = ['#f4a261', '#2a9d8f', '#e76f51', '#264653', '#ffb703'];
+
+
+button.addEventListener('click', () => {
+  
+  const randomColor = colors[Math.floor(Math.random() * colors.length)];
+  document.body.style.backgroundColor = randomColor;
+});
+
